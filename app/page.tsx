d1ca713bb38a8790d807edcc05ea1e1124652f80
yangji721 <yangji12138@gmail.com>
2026-04-20 09:02:57 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import AboutSection from '@/components/AboutSection';
 import LatestNewsSection from '@/components/LatestNewsSection';
 import ProjectSection from '@/components/ProjectSection';
 import SelectedPublicationsSection from '@/components/SelectedPublicationsSection';
-import bibtex from '@/data/publications/Publications.bib';
+import bibtex from '@/data/publications/SelectedPublications.bib';
 import { homepageSection } from '@/data/website.config';
 
 export default function Page() {
